85c5e78c9631e432c008f5e97cbf1908028cb827
Dirk Esser <dirk.esser@deterministic-arts.de>
2016-04-16 18:49:07 +0200

    Add initial version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# DartsCLUUID
-Provides support for UUIDs as proper values
+# UUID Library
+
+This is a simple library to work with UUID values. The library primarily
+uses the Leach-Salz variant of UUIDs, but can cope with essentially all
+kinds of UUIDs.
+
+- Type `uuid`
+- Function `uuid` _object_ &rarr; _uuid_
+- Function `uuidp` _object_ &rarr; _boolean_
+- Function `uuid=` _object1_ _object2_ &rarr; _boolean_
+- Function `uuid/=` _object1_ _object2_ &rarr; _boolean_
+- Function `uuid<=` _object1_ _object2_ &rarr; _boolean_
+- Function `uuid<` _object1_ _object2_ &rarr; _boolean_
+- Function `uuid>=` _object1_ _object2_ &rarr; _boolean_
+- Function `uuid>` _object1_ _object2_ &rarr; _boolean_
+- Function `print-uuid` _object_ `&key` _stream_ _braces_ _downcase_ &rarr; _answer_
+- Function `uuid-bytes` _object_ &rarr; _array_
+- Function `uuid-string` _object_ `&key` _braces_ _downcase_ &rarr; _string_
+- Function `uuid-number` _object_ &rarr; _value_
+- Function `uuid-version` _object_ &rarr; _value_
+- Function `uuid-variant` _object_ &rarr; _value_
+- Function `uuid-node` _object_ &rarr; _variant_
+- Function `uuid-clock-sequence` _object_ &rarr; _variant_
+- Function `uuid-timestamp` _object_ &rarr; _variant_
+- Function `parse-uuid` string `&key` _start_ _end_ &rarr; _object_
+- Function `random-uuid` `&key` _random-state_ &rarr; _object_
+- Function `uuid-for-name` _string_ `&key` _start_ _end_ &rarr; _object_
---
+++ src/uuid.lisp
@@ -0,0 +1,213 @@
+#|                                           -*- mode: lisp; coding: utf-8 -*-
+  Deterministic Arts -- UUID support
+  Copyright (c) 2016 Dirk Esser
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+|#
+
+(in-package "DARTS.LIB.UUID")
+
+(defstruct (uuid (:copier nil) (:constructor %make-uuid (low-word high-word))
+                 (:predicate uuidp))
+  (low-word (error "missing low word") :type (unsigned-byte 64) :read-only t)
+  (high-word (error "missing high word") :type (unsigned-byte 64) :read-only t)
+  (%hash -1 :type fixnum))
+
+(defun uuid= (u1 u2)
+  (and (eql (uuid-low-word u1) (uuid-low-word u2))
+       (eql (uuid-high-word u1) (uuid-high-word u2))))
+
+(defun uuid< (u1 u2)
+  (let ((h1 (uuid-high-word u1)) 
+        (h2 (uuid-high-word u2)))
+    (cond
+      ((< h1 h2) t)
+      ((< h2 h1) nil)
+      (t (< (uuid-low-word u1)
+            (uuid-low-word u2))))))
+
+(defun uuid-hash (object)
+  (let ((value (uuid-%hash object)))
+    (if (>= value 0) value
+        (setf (uuid-%hash object)
+              (mod (logxor (uuid-high-word object) (uuid-low-word object))
+                   most-positive-fixnum)))))
+
+(declaim (inline uuid/= uuid<= uuid>= uuid>))
+
+(defun uuid/= (u1 u2)
+  (not (uuid= u1 u2)))
+
+(defun uuid> (u1 u2)
+  (uuid< u2 u1))
+
+(defun uuid>= (u1 u2)
+  (not (uuid< u1 u2)))
+
+(defun uuid<= (u1 u2)
+  (not (uuid< u2 u1)))
+
+(defun print-uuid (object &key (stream *standard-output*) braces downcase)
+  (let ((high (uuid-high-word object))
+        (low (uuid-low-word object))
+        (digits (if downcase "0123456789abcdef" "0123456789ABCDEF")))
+    (labels
+        ((dash () (write-char #\- stream))
+         (digits (number count)
+           (loop
+             :for k :downfrom (1- count) :to 0
+             :do (write-char (char digits (ldb (byte 4 (* 4 k)) number)) stream))))
+      (when braces (write-char #\{ stream))
+      (digits (ash high -32) 8)
+      (dash)
+      (digits (ash high -16) 4)
+      (dash)
+      (digits high 4)
+      (dash)
+      (digits (ash low -48) 4)
+      (dash)
+      (digits low 12)
+      (when braces (write-char #\} stream))
+      object)))
+
+(defmethod print-object ((object uuid) stream)
+  (if (not *print-escape*)
+      (print-uuid object :stream stream :braces t)
+      (print-unreadable-object (object stream :type t :identity nil)
+        (print-uuid object :stream stream))))
+
+(defun uuid-bytes (object)
+  (let ((array (make-array 16 :element-type '(unsigned-byte 8))))
+    (flet ((emplace (number pointer)
+             (loop
+               :for k :downfrom 56 :to 0 :by 8 
+               :for p :upfrom pointer
+               :do (setf (aref array p) (ldb (byte 8 k) number)))))
+      (emplace (uuid-high-word object) 0)
+      (emplace (uuid-low-word object) 8)
+      array)))
+
+(defun uuid-number (object)
+  (logior (ash (uuid-high-word object) 64) 
+          (uuid-low-word object)))
+
+(defun uuid-version (object)
+  (logand #x0f (ash (uuid-high-word object) -12)))
+
+(defun uuid-variant (object)
+  (let ((byte (ldb (byte 8 56) (uuid-low-word object))))
+    (cond
+      ((zerop (logand byte #b10000000)) :ncs)
+      ((eql (logand byte #b11000000) #b10000000) :leach-salz)
+      ((eql (logand byte #b11100000) #b11000000) :microsoft)
+      (t :reserved))))
+
+(defun uuid-timestamp (object)
+  (and (eql (uuid-version object) 1)
+       (logior (ash (logand (uuid-high-word object) #x0FFF) 48)
+               (ash (logand (ash (uuid-high-word object) -16) #xFFFF) 32)
+               (ash (uuid-high-word object) -32))))
+
+(defun uuid-clock-sequence (object)
+  (and (eql (uuid-version object) 1)
+       (ash (logand (uuid-low-word object) #x3FFF000000000000) -48)))
+
+(defun uuid-node (object)
+  (and (eql (uuid-version object) 1)
+       (logand (uuid-low-word object) #xFFFFFFFFFFFF)))
+
+
+(defun uuid-string (object &key braces downcase)
+  (with-output-to-string (stream)
+    (print-uuid object 
+                :stream stream 
+                :braces braces 
+                :downcase downcase)))
+
+
+(defun parse-uuid (value &key (start 0) end)
+  (let* ((string (string value))
+         (end (or end (length string))))
+    (when (and (< start end) (eql (char string start) #\{))
+      (incf start)
+      (unless (and (< start end) (eql (char string (1- end)) #\}))
+        (return-from parse-uuid nil))
+      (decf end))
+    (multiple-value-bind (match-start match-end group-starts group-ends) (scan "^([0-9a-fA-F]{1,8})-([0-9a-fA-F]{1,4})-([0-9a-fA-F]{1,4})-([0-9a-fA-F]{1,4})-([0-9a-fA-F]{1,12})$" 
+                                                                               string :start start :end end)
+      (declare (ignore match-end))
+      (and match-start
+           (flet ((parse-group (k)
+                    (parse-integer string :start (aref group-starts k) :end (aref group-ends k) :radix 16)))
+             (%make-uuid 
+              (logior (ash (parse-group 3) 48)
+                      (parse-group 4))
+              (logior (ash (parse-group 0) 32)
+                      (ash (parse-group 1) 16)
+                      (parse-group 2))))))))
+
+(defun uuid (object)
+  (labels
+      ((bad-value ()
+         (error 'simple-type-error
+                :datum object :expected-type 'uuid
+                :format-control "~S is not a well-formed UUID value" 
+                :format-arguments (list object))))
+    (typecase object
+      (uuid object)
+      (string (or (parse-uuid object) (bad-value)))
+      ((unsigned-byte 128) (%make-uuid (logand object #xFFFFFFFFFFFFFFFF) (ash object -64)))
+      ((array (unsigned-byte 8) (16)) 
+       (let ((high 0) (low 0))
+         (loop
+           :for k :upfrom 0
+           :for j :downfrom 56 :to 0 :by 8
+           :do (setf (ldb (byte 8 j) high) (aref object k)))
+         (loop
+           :for k :upfrom 8 
+           :for j :downfrom 56 :to 0 :by 8
+           :do (setf (ldb (byte 8 j) low) (aref object k)))
+         (%make-uuid low high)))
+      (t (bad-value)))))
+
+
+(defun set-version (buffer version)
+  (declare (type (array (unsigned-byte 8) (16)) buffer)
+           (type (unsigned-byte 8) version))
+  (setf (aref buffer 6) (logior (logand (aref buffer 6) #x0f) version))
+  (setf (aref buffer 8) (logior (logand (aref buffer 8) #x3f) #x80))
+  buffer)
+
+
+(defun random-uuid (&key (random-state *random-state*))
+  (let ((buffer (make-array 16 :element-type '(unsigned-byte 8))))
+    (loop
+      :for k :upfrom 0 :below 16
+      :do (setf (aref buffer k) (random 256 random-state)))
+    (uuid (set-version buffer #x40))))
+
+
+(defun uuid-for-name (string &key (start 0) end)
+  (let* ((string (string string))
+         (end (or end (length string)))
+         (bytes (string-to-utf-8-bytes (subseq string start end) :null-terminate nil))
+         (buffer (let ((digest (make-digest :md5)))
+                   (update-digest digest bytes)
+                   (produce-digest digest))))
+    (uuid (set-version buffer #x30))))
